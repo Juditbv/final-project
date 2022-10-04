2600c01b7d2431eeb97f3a7708ba8bb2d6f28b25
Judit Bancells Vélez <judit@juditbancells.cat>
2022-10-04 17:50:29 +0200

    Customize tailwind properties

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      blue: "#758ECD",
+      green: "#91C7B1",
+      yellow: "#F2C078",
+      red: "EF6F6C",
+      neutral: "#54494B",
+    },
+    fontFamily: {
+      sans: ["Poppins", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
