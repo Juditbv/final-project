c6f113a318ec3417ef3a798fbe6515b528eaece4
Judit Bancells Vélez <judit@juditbancells.cat>
2022-10-06 13:08:21 +0200

    finish nav component dashboard

--- src/stores/user.js
+++ src/stores/user.js
@@ -31,6 +31,14 @@ export const useUserStore = defineStore("user", {
         console.log(this.user);
       }
     },
+    async signOut() {
+      const { error } = await supabase.auth.signOut();
+      if (error) throw error;
+      // if (user) {
+      //   this.user = user;
+      //   console.log(this.user);
+      // }
+    },
     persist: {
       enabled: true,
       strategies: [
