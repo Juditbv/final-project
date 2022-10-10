ef6e3cdb4e2d6b230b0b86b63ac0b97ee050d71f
Judit Bancells Vélez <judit@juditbancells.cat>
2022-10-10 18:42:53 +0200

    REMOVE hints comments

--- src/components/TaskItem.vue
+++ src/components/TaskItem.vue
@@ -24,14 +24,6 @@
 
 	const taskTitle = ref(props.task.title);
 	const taskDescription = ref(props.task.description);
-
-	const iconComplete = ref(
-		`<svg xmlns='http://www.w3.org/2000/svg'fill='none' viewBox='0 0 24 24' stroke-width='1.5' class='w-4 h-4 stroke-[white]'><path stroke-linecap='round' stroke-linejoin='round' d='M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3'/></svg>`
-	);
-
-	const iconNotComplete = ref(
-		`<svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5'  class='w-4 h-4 stroke-[white]'><path stroke-linecap='round' stroke-linejoin='round' d='M4.5 12.75l6 6 9-13.5'/></svg>`
-	);
 </script>
 
 <template>
@@ -79,11 +71,24 @@
 				class="rounded-full"
 				:class="
 					task.is_complete
-						? 'bg-blue hover:bg-neutral p-2'
+						? 'bg-green p-2'
 						: 'border border-green hover:bg-green p-[7px]'
 				"
-				v-html="task.is_complete ? iconComplete : iconNotComplete"
-			></button>
+			>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					class="w-4 h-4 stroke-[white]"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M4.5 12.75l6 6 9-13.5"
+					/>
+				</svg>
+			</button>
 			<button
 				v-if="!task.is_complete"
 				@click="editingTask = !editingTask"
@@ -141,15 +146,3 @@
 		@apply px-3 py-1 mb-2 border border-neutral outline-neutral rounded-xl w-full max-w-xs;
 	}
 </style>
-
-<!-- 
-**Hints**
-1. ref() or reactive() can be used here to store the following, think if you want to store them either individually or like an object, up to you.
-2. Data properties need here are the following: a boolean to store a false**Important variable, a string to store an error, a string to store the value of the task that the user can edit, an initial false boolean to hide the inputFIeld used to edit the new task detail or details[this is in reference of the task title and the task description].
-3. Store the custom emit events that will be used to call the functions of the homeView for editing, deleting and toggling the status[completed, not complted] of the taskItem.
-4. Function to handle the error with the data properties used to control the error and the the boolean controlling the boolean empty variable.
-5. Function to handle the edit dialogue where the inputField is displayed and the string used to store the value of the inputField will be used here to save the value as a prop on this function.
-6. Function to emmit a custom event emit() that takes 2 parameters a name for the custom event and the value that will be send via the prop to the parent component. This function can control the toggle completion of the task on the homeview.
-7. Function to edit the task information that you decided that the user can edit. This function's body takes in a conditional that first checks if the value of the task [either title and description or just title] is empty which if true it runs the function used to handle the error on hint4. Else, the conditional sets the first mentioned boolean data property on hint2 back to its inital boolean value to hide the error message and repeat the same for the data property mentioned 4th on hint2; a constant that stores an object that holds the oldValue from the prop item and the value of the task coming from the data property mentioned 3rd on hint2; a custom event emit() that takes 2 parameters a name for the custom event and the value from the object used on this part of the conditional and lastly this part of the conditional sets the value of input field to an empty string to clear it from the ui. 
-8. Function to emmit a custom event emit() that takes 2 parameters a name for the custom event and the value that will be send via the prop to the parent component. This function can control the removal of  the task on the homeview.
--->
