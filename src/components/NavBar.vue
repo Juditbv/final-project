aed687f5027a0bf5c4c604870f02ef7989a6da86
Judit Bancells Vélez <judit@juditbancells.cat>
2022-10-13 18:32:20 +0200

    Refactor logout button mobile

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -47,7 +47,23 @@
 			<span class="hidden md:inline-flex">The</span>
 			<span v-html="logoTodo"></span> of {{ nameEmail }}
 		</p>
-		<button @click="logOut">Log out</button>
+		<button class="hidden md:inline-flex" @click="logOut">Log out</button>
+		<button class="md:hidden" @click="logOut">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="white"
+				class="w-6 h-6"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M5.636 5.636a9 9 0 1012.728 0M12 3v9"
+				/>
+			</svg>
+		</button>
 	</div>
 </template>
 
