7fe802bcf52a0619f0d7ac805d2cbf2ed5ce44e5
Judit Bancells <judit@intuix.cat>
2022-11-16 17:28:38 +0100

    ADD initial sentence if no tasks

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,288 +1,295 @@
 <script setup>
-	import NavBar from "../components/NavBar.vue";
-	import NewTask from "../components/NewTask.vue";
-	import TaskItem from "../components/TaskItem.vue";
-	import FooterComp from "../components/FooterComp.vue";
-	import UserPreferences from "../components/UserPreferences.vue";
-	import ScrollToTop from "../components/ScrollToTop.vue";
+  import NavBar from "../components/NavBar.vue";
+  import NewTask from "../components/NewTask.vue";
+  import TaskItem from "../components/TaskItem.vue";
+  import FooterComp from "../components/FooterComp.vue";
+  import UserPreferences from "../components/UserPreferences.vue";
+  import ScrollToTop from "../components/ScrollToTop.vue";
 
-	import { ref, onMounted, computed, onBeforeMount } from "vue";
-	import { useTaskStore } from "../stores/task";
-	import { supabase } from "../supabase";
+  import { ref, onMounted, computed } from "vue";
+  import { useTaskStore } from "../stores/task";
 
-	const storeTasks = useTaskStore();
+  const storeTasks = useTaskStore();
 
-	onMounted(() => {
-		initialFetch();
-		window.addEventListener("resize", changeViewCols);
-	});
+  onMounted(() => {
+    initialFetch();
+    window.addEventListener("resize", changeViewCols);
+  });
 
-	async function pushTaskSup(title, description, priority) {
-		try {
-			await storeTasks.addTask(title, description, priority);
-			initialFetch();
-		} catch (error) {
-			errorMsg.value = "There's been an error creating your task:" + error;
-		}
-	}
+  async function pushTaskSup(title, description, priority) {
+    try {
+      await storeTasks.addTask(title, description, priority);
+      initialFetch();
+    } catch (error) {
+      errorMsg.value = "There's been an error creating your task:" + error;
+    }
+  }
 
-	const deleteTask = async (taskId) => {
-		try {
-			await storeTasks.deleteTask(taskId);
-			initialFetch();
-		} catch (error) {
-			errorMsg.value = "There's been an error deleting your task:" + error;
-		}
-	};
+  const deleteTask = async taskId => {
+    try {
+      await storeTasks.deleteTask(taskId);
+      initialFetch();
+    } catch (error) {
+      errorMsg.value = "There's been an error deleting your task:" + error;
+    }
+  };
 
-	const completeTask = async (taskId, status) => {
-		try {
-			await storeTasks.toggleCompleteTask(taskId, status);
-			initialFetch();
-		} catch (error) {
-			errorMsg.value = "There's been an error completing your task:" + error;
-		}
-	};
+  const completeTask = async (taskId, status) => {
+    try {
+      await storeTasks.toggleCompleteTask(taskId, status);
+      initialFetch();
+    } catch (error) {
+      errorMsg.value = "There's been an error completing your task:" + error;
+    }
+  };
 
-	const updateTask = async (taskId, payload) => {
-		try {
-			await storeTasks.updateTask(taskId, payload);
-			initialFetch();
-		} catch (error) {
-			errorMsg.value = "There's been an error while updating your task:" + error;
-		}
-	};
+  const updateTask = async (taskId, payload) => {
+    try {
+      await storeTasks.updateTask(taskId, payload);
+      initialFetch();
+    } catch (error) {
+      errorMsg.value =
+        "There's been an error while updating your task:" + error;
+    }
+  };
 
-	const viewCols = ref(false);
+  const viewCols = ref(false);
 
-	const changeView = (view) => {
-		viewCols.value = view;
-	};
+  const changeView = view => {
+    viewCols.value = view;
+  };
 
-	const changeViewCols = () => {
-		if (window.innerWidth < 768) {
-			viewCols.value = false;
-		}
-	};
+  const changeViewCols = () => {
+    if (window.innerWidth < 768) {
+      viewCols.value = false;
+    }
+  };
 
-	const showCompletedTasks = ref(true);
+  const showCompletedTasks = ref(true);
 
-	const showCompleted = (value) => {
-		showCompletedTasks.value = value;
-	};
-	console.log(showCompletedTasks.value);
+  const showCompleted = value => {
+    showCompletedTasks.value = value;
+  };
 
-	const tasksCompleted = computed(() => {
-		return storeTasks.tasks.filter((task) => task.is_complete);
-	});
+  const tasksCompleted = computed(() => {
+    return storeTasks.tasks.filter(task => task.is_complete);
+  });
 
-	const tasksPending = computed(() => {
-		return storeTasks.tasks.filter((task) => !task.is_complete);
-	});
-	const allDates = computed(() => {
-		storeTasks.fetchOnlyDates();
-		return storeTasks.dates;
-	});
+  const tasksPending = computed(() => {
+    return storeTasks.tasks.filter(task => !task.is_complete);
+  });
+  const allDates = computed(() => {
+    storeTasks.fetchOnlyDates();
+    return storeTasks.dates;
+  });
 
-	const errorMsg = ref("");
+  const errorMsg = ref("");
+  const order = ref("");
 
-	const order = ref("");
+  // const allTasks =
+  const sortPriority = async () => {
+    order.value = "priority";
+    if (filterNumber.value !== "0" || filterDateValue.value !== "all") {
+      await storeTasks.filterTasks(
+        filterNumber.value,
+        filterDateValue.value,
+        order.value
+      );
+    } else {
+      await storeTasks.fetchTasksPriority();
+    }
+  };
 
-	// const allTasks =
-	const sortPriority = async () => {
-		order.value = "priority";
-		if (filterNumber.value !== "0" || filterDateValue.value !== "all") {
-			await storeTasks.filterTasks(
-				filterNumber.value,
-				filterDateValue.value,
-				order.value
-			);
-		} else {
-			await storeTasks.fetchTasksPriority();
-		}
-	};
+  const sortDate = async () => {
+    order.value = "inserted_at";
+    if (filterNumber.value !== "0" || filterDateValue.value !== "all") {
+      await storeTasks.filterTasks(
+        filterNumber.value,
+        filterDateValue.value,
+        order.value
+      );
+    } else {
+      await storeTasks.fetchTasks();
+    }
+  };
 
-	const sortDate = async () => {
-		order.value = "inserted_at";
-		if (filterNumber.value !== "0" || filterDateValue.value !== "all") {
-			await storeTasks.filterTasks(
-				filterNumber.value,
-				filterDateValue.value,
-				order.value
-			);
-		} else {
-			await storeTasks.fetchTasks();
-		}
-	};
+  const initialFetch = () => {
+    if (order.value === "priority") {
+      sortPriority();
+    } else {
+      sortDate();
+    }
+  };
 
-	const initialFetch = () => {
-		if (order.value === "priority") {
-			sortPriority();
-		} else {
-			sortDate();
-		}
-	};
+  const filterNumber = ref("0");
+  const filterDateValue = ref("all");
+  const filtersApplied = ref(false);
 
-	const filterNumber = ref("0");
-	const filterDateValue = ref("all");
-
-	const filterTasks = async () => {
-		await storeTasks.filterTasks(
-			filterNumber.value,
-			filterDateValue.value,
-			order.value
-		);
-	};
+  const filterTasks = async () => {
+    await storeTasks.filterTasks(
+      filterNumber.value,
+      filterDateValue.value,
+      order.value
+    );
+  };
 </script>
 
 <template>
-	<nav>
-		<NavBar />
-	</nav>
-	<main
-		class="
-			p-8
-			grid grid-cols-1
-			gap-10
-			auto-rows-max
-			lg:auto-rows-fr lg:grid-cols-3
-			min-height-empty
-		"
-	>
-		<div>
-			<div class="lg:sticky top-8">
-				<UserPreferences
-					@toggle-list="changeView"
-					@toggle-cols="changeView"
-					@order-priority="sortPriority"
-					@order-date="sortDate"
-					@show-completed="showCompleted"
-					@dont-show-completed="showCompleted"
-					:view-cols="viewCols"
-					:order="order"
-					:completed="showCompletedTasks"
-				/>
-				<NewTask @new-task="pushTaskSup" />
-			</div>
-		</div>
-		<section class="lg:col-span-2">
-			<div class="flex items-end flex-wrap justify-between">
-				<h1 class="font-semibold text-6xl">Your tasks</h1>
-				<div class="flex flex-wrap mt-3">
-					<span class="ml-0 sm:ml-4 font-semibold">Filter by</span>
-					<div class="w-full sm:max-w-max mt-2 sm:mt-0">
-						<label class="text-sm ml-0 sm:ml-5">Priority</label>
-						<select
-							name="priority"
-							id="priority"
-							class="ml-2 mr-5 text-sm min-w-[75%] sm:min-w-max"
-							placeholder="Priority"
-							@change="filterTasks"
-							v-model="filterNumber"
-						>
-							<option value="0" selected>All</option>
-							<option value="1">Low</option>
-							<option value="2">Medium</option>
-							<option value="3">High</option>
-						</select>
-					</div>
-					<span class="mx-2 mt-2 sm:mt-0 text-sm leading-7">and</span>
-					<div class="w-full sm:max-w-max mt-2 sm:mt-0">
-						<label class="text-sm ml-0 sm:ml-5">Date</label>
-						<select
-							name="date"
-							id="date"
-							class="ml-2 text-sm min-w-[80%] sm:min-w-max"
-							@change="filterTasks"
-							v-model="filterDateValue"
-						>
-							<option value="all" selected>All</option>
-							<option v-for="date in allDates" :key="date" :value="date">
-								{{ date }}
-							</option>
-						</select>
-					</div>
-				</div>
-			</div>
-			<section
-				class="grid gap-10 mt-10"
-				:class="viewCols ? 'grid-cols-2' : 'grid-cols-1'"
-			>
-				<TaskItem
-					v-for="task in tasksPending"
-					:key="task.id"
-					:task="task"
-					@delete-task="deleteTask"
-					@complete-task="completeTask"
-					@updateContTask="updateTask"
-				/>
-			</section>
-			<h5
-				v-if="tasksCompleted.length > 0 && showCompletedTasks"
-				class="font-semibold text-2xl mt-10"
-			>
-				Here are your completed tasks
-			</h5>
-			<h5
-				v-if="tasksCompleted.length <= 0 && tasksPending.length <= 0"
-				class="text-lg mt-10"
-			>
-				There are no tasks matching your filters. Try with others ;)
-			</h5>
-			<section
-				v-if="tasksCompleted.length > 0 && showCompletedTasks"
-				class="grid gap-10 mt-10"
-				:class="viewCols ? 'grid-cols-2' : 'grid-cols-1'"
-			>
-				<TaskItem
-					v-for="task in tasksCompleted"
-					:key="task.id"
-					:task="task"
-					@delete-task="deleteTask"
-					@complete-task="completeTask"
-				/>
-			</section>
-			<div v-if="errorMsg" class="notificationHome">
-				<p>
-					<span
-						><svg
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke-width="1.5"
-							stroke="#EF6F6C"
-							class="w-6 h-6 inline"
-						>
-							<path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z"
-							/>
-						</svg>
-					</span>
-					{{ errorMsg }}
-				</p>
-			</div>
-		</section>
-	</main>
-	<FooterComp />
-	<ScrollToTop />
+  <nav>
+    <NavBar />
+  </nav>
+  <main
+    class="p-8 grid grid-cols-1 gap-10 auto-rows-max lg:auto-rows-fr lg:grid-cols-3 min-height-empty"
+  >
+    <div>
+      <div class="lg:sticky top-8">
+        <UserPreferences
+          @toggle-list="changeView"
+          @toggle-cols="changeView"
+          @order-priority="sortPriority"
+          @order-date="sortDate"
+          @show-completed="showCompleted"
+          @dont-show-completed="showCompleted"
+          :view-cols="viewCols"
+          :order="order"
+          :completed="showCompletedTasks"
+        />
+        <NewTask @new-task="pushTaskSup" />
+      </div>
+    </div>
+    <section class="lg:col-span-2">
+      <div class="flex items-end flex-wrap justify-between">
+        <h1 class="font-semibold text-6xl">Your tasks</h1>
+        <div class="flex flex-wrap mt-3">
+          <span class="ml-0 sm:ml-4 font-semibold">Filter by</span>
+          <div class="w-full sm:max-w-max mt-2 sm:mt-0">
+            <label class="text-sm ml-0 sm:ml-5">Priority</label>
+            <select
+              name="priority"
+              id="priority"
+              class="ml-2 mr-5 text-sm min-w-[75%] sm:min-w-max"
+              placeholder="Priority"
+              @change="filterTasks"
+              v-model="filterNumber"
+            >
+              <option value="0" selected>All</option>
+              <option value="1">Low</option>
+              <option value="2">Medium</option>
+              <option value="3">High</option>
+            </select>
+          </div>
+          <span class="mx-2 mt-2 sm:mt-0 text-sm leading-7">and</span>
+          <div class="w-full sm:max-w-max mt-2 sm:mt-0">
+            <label class="text-sm ml-0 sm:ml-5">Date</label>
+            <select
+              name="date"
+              id="date"
+              class="ml-2 text-sm min-w-[80%] sm:min-w-max"
+              @change="filterTasks"
+              v-model="filterDateValue"
+            >
+              <option value="all" selected>All</option>
+              <option v-for="date in allDates" :key="date" :value="date">
+                {{ date }}
+              </option>
+            </select>
+          </div>
+        </div>
+      </div>
+      <section
+        class="grid gap-10 mt-10"
+        :class="viewCols ? 'grid-cols-2' : 'grid-cols-1'"
+      >
+        <TaskItem
+          v-for="task in tasksPending"
+          :key="task.id"
+          :task="task"
+          @delete-task="deleteTask"
+          @complete-task="completeTask"
+          @updateContTask="updateTask"
+        />
+      </section>
+      <h5
+        v-if="tasksCompleted.length > 0 && showCompletedTasks"
+        class="font-semibold text-2xl mt-10"
+      >
+        Here are your completed tasks
+      </h5>
+      <h5
+        v-if="
+          tasksCompleted.length === 0 &&
+          tasksPending.length === 0 &&
+          (filterNumber !== '0' || filterDateValue !== 'all')
+        "
+        class="text-lg mt-10"
+      >
+        There are no tasks matching your filters. Try with others ;)
+      </h5>
+      <h5
+        v-if="
+          tasksPending.length === 0 &&
+          tasksCompleted.length === 0 &&
+          filterNumber === '0' &&
+          filterDateValue === 'all'
+        "
+        class="text-lg mt-10"
+      >
+        Come on! Start planning tasks here ;)
+      </h5>
+      <section
+        v-if="tasksCompleted.length > 0 && showCompletedTasks"
+        class="grid gap-10 mt-10"
+        :class="viewCols ? 'grid-cols-2' : 'grid-cols-1'"
+      >
+        <TaskItem
+          v-for="task in tasksCompleted"
+          :key="task.id"
+          :task="task"
+          @delete-task="deleteTask"
+          @complete-task="completeTask"
+        />
+      </section>
+      <div v-if="errorMsg" class="notificationHome">
+        <p>
+          <span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="#EF6F6C"
+              class="w-6 h-6 inline"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z"
+              />
+            </svg>
+          </span>
+          {{ errorMsg }}
+        </p>
+      </div>
+    </section>
+  </main>
+  <FooterComp />
+  <ScrollToTop />
 </template>
 
 <style>
-	body {
-		@apply bg-neutral bg-opacity-[15%];
-	}
+  body {
+    @apply bg-neutral bg-opacity-[15%];
+  }
 
-	.min-height-empty {
-		min-height: calc(100vh - 80px);
-	}
+  .min-height-empty {
+    min-height: calc(100vh - 80px);
+  }
 
-	.notificationHome {
-		@apply bg-red bg-opacity-70 border border-red mb-4 rounded-lg text-left p-2 text-sm absolute bottom-2 left-2 max-w-xl;
-	}
+  .notificationHome {
+    @apply bg-red bg-opacity-70 border border-red mb-4 rounded-lg text-left p-2 text-sm absolute bottom-2 left-2 max-w-xl;
+  }
 
-	select {
-		@apply max-w-max w-full border-b bg-neutral bg-opacity-0 text-neutral text-opacity-70 focus:outline-none pt-3 pb-1;
-	}
+  select {
+    @apply max-w-max w-full border-b bg-neutral bg-opacity-0 text-neutral text-opacity-70 focus:outline-none pt-3 pb-1;
+  }
 </style>
